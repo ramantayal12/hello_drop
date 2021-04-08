31d538c5f850bbb967cafd7131838c128380598b
ramantayal12 <ramantayal12@gmail.com>
2021-04-08 11:00:11 +0530

    1. Added Post request that works

--- src/main/java/com/example/representations/contact.java
+++ src/main/java/com/example/representations/contact.java
@@ -2,27 +2,11 @@ package com.example.representations;
 
 public class contact {
 
-    private int id;
-    private String name;
+    int id;
+    String name;
 
-    public contact(int id, String name ) {
-        this.id = 0;
-        this.name = null;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
+    public contact(int id, String name) {
         this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
         this.name = name;
     }
 }
--- src/main/java/com/example/resources/contact_resource_class.java
+++ src/main/java/com/example/resources/contact_resource_class.java
@@ -4,14 +4,17 @@ package com.example.resources;
 import com.codahale.metrics.annotation.Timed;
 import com.example.representations.contact;
 
+import javax.swing.text.html.parser.Entity;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.List;
 
 @Path("/events")
 @Produces(MediaType.APPLICATION_JSON)
 public class contact_resource_class {
 
+    int id;
     @GET
     @Timed
     public String say_hi(){
@@ -25,16 +28,13 @@ public class contact_resource_class {
         return Response.ok(new contact(id,"Raman")).build();
     }
 
-    /*
-    PUT method used to insert data in web application
-
-    @PUT
-    @Path("/{id}")
-    public Response.ResponseBuilder updateContact(@PathParam("id") int id){
-        return Response.ok(new contact(id,"New name "));
+    @POST
+    @Path("update/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public int updateRecord(@PathParam("id") int id, List<Entity> entities) {
+        // Do something with entities...
+        this.id = id;
+        return 0;
     }
-
-
-     */
 
 }
